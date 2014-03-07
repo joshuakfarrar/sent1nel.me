313a16385c3276eb02a0a7f8763ddacd6186b25c
Joshua Farrar <joshua.farrar@charity.org>
2014-03-06 22:08:59 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+<title>Joshua Farrar - Bringing Technology and Philanthropy Together</title>
+	<meta charset="utf-8" />
+	<meta name="description" content="Joshua Farrar is creating new technology to power global nonprofits. He's a husband, developer, entrepreneur, philanthropist, and drinks too much coffee." />
+	<meta name="keywords" content="Joshua Farrar, developer, Washington DC, Arlington, Virginia, United States, UX, usability, typography, css, html, jQuery, user experience, node.js, drupal, ruby on rails" />
+	<meta name="author" content="Joshua Farrar" />
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="apple-touch-icon" href="http://sent1nel.me/images/apple-touch-icon.png?v=654312" />
+	<link rel="Shortcut Icon" href="http://sent1nel.me/images/favicon.ico?v=654312" type="image/x-icon" />
+	<link rel="stylesheet" href="css/style.css?v=654313" type="text/css" media="screen" />
+	<script type="text/javascript" src="http://sent1nel.me/js/jquery-1.6.2.min.js?v=654312"></script>
+	<script type="text/javascript" src="http://sent1nel.me/vendor/vegas/src/jquery.vegas.js"></script>
+		<script type="text/javascript">
+		  $(document).ready(function() { 
+				$(".content").css("display", "none");
+				$(".content").fadeIn(1500);
+			});
+		</script>
+</head>	
+<body>
+<div class="content">
+	<div class="hero-section">
+		<p class="hero"><a href="http://twitter.com/sent1nel" class="circle">
+	<img src="http://www.gravatar.com/avatar/366d95ab4984162d53d9477d17850297.png" alt="Joshua Farrar">
+</a></p>
+		<p><strong>I'm Joshua</strong>, developer, UX, product designer &amp; maker of technology for good.</p>
+		<ul class="social">
+			<li class="twitter"><a href="http://twitter.com/sent1nel" title="Twitter"></a></li>
+			<li class="github"><a href="http://github.com/sent1nel" title="Github"></a></li>
+			<li class="email"><a href="mailto:joshua@sent1nel.me" title="Email"></a></li>
+		</ul>
+	</div>
+</div>
+<script type="text/javascript">
+$( function() {
+	$.vegas('slideshow', {
+		delay:7500,
+		opacity:0.5,
+		backgrounds:[
+			{ src:'http://sent1nel.me/images/car1.jpg' }
+		]
+		})('overlay', {
+			src:'http://sent1nel.me/vendor/vegas/dist/overlays/01.png',
+			opacity:0.4
+		});
+	});
+</script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48739531-1', 'sent1nel.me');
+  ga('send', 'pageview');
+
+</script>
+</body>	
+</html>
